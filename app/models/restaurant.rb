86ae9296c37e2bf74da1e8fa10a963841baba4f4
rodrigo cabral <cabral-rodrigo@hotmail.com>
2019-02-13 14:32:12 -0300

    Restaurant and review models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  CATEGORY = %w(chinese italian japanese french belgian)
+  has_many :reviews, dependent: :destroy
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+
+end
