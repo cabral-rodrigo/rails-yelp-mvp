2c32142f59dc2ed84391f08ef4d5783b267cb410
rodrigo cabral <cabral-rodrigo@hotmail.com>
2019-02-13 15:30:03 -0300

    restaurants and views routes defined

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,29 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
   def index
+    @restaurant = Restaurant.all
   end
 
   def show
+
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurants_params)
+  end
+
+  private
+
+  def restaurants_params
+    params.require(@restaurant).permit(:name, :address, :phone_number, :category, :reviews)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
