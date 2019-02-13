6dbc022e72d0431cbc75a1607b14c113875f0242
rodrigo cabral <cabral-rodrigo@hotmail.com>
2019-02-13 14:48:44 -0300

    database with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Wok',
+    address:      '7 Boundary St, Buenos aires E2 7JE',
+    phone_number: '0381-123-123-456',
+    category:     'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '251 Shoreditch High St, Buenos aires E1 6PQ',
+    phone_number: '011-123-123-789',
+    category:     'italian',
+  },
+  {
+    name:         'Mc Donalds',
+    address:      '314 Buenavista High St, buenos Aires E1 6PQ',
+    phone_number: '011-123-123-333',
+    category:     'belgian',
+  },
+  {
+    name:         'Sushi One',
+    address:      '281A Av. Avellaneda High St, Buenos aires E1 6PQ',
+    phone_number: '011-123-123-444',
+    category:     'japanese',
+  },
+  {
+    name:         'Cocu',
+    address:      '24B Shoreditch High St, Buenos aires E1 6PQ',
+    phone_number: '011-123-123-123',
+    category:     'french',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
